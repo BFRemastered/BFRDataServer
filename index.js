aac1be2052e6e6668fc901ca874926c9eb733403
Midas <102895212+generic-glitch@users.noreply.github.com>
2022-08-10 20:07:17 +0000

    Created an alternative event when a skin isn't found.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const crypto = require('crypto');
+const fs = require('fs')
 const app = express();
 
 app.use(fileUpload({
@@ -10,7 +11,20 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.get("/skin/:id", (req, res) => {
-res.sendFile(__dirname+"/files/skins/"+req.params.id);
+  try {
+    if (fs.existsSync(__dirname+"/files/skins/"+req.params.id)) {
+      res.sendFile(__dirname+"/files/skins/"+req.params.id);
+    }
+    else{
+      console.log("skin not found");
+      res.sendStatus(404);
+      res.send("not found");
+    }
+  } catch(err) {
+    console.log("skin not found");
+    res.sendStatus(404);
+    res.send("not found");
+  }
 });
 app.get("/", (req, res) => {
  res.json({"res": "Successful connection to BFR"});
